97a0d11693b464c7ef02b7ef715288b8d466125c
ozur2k <ozur1233@gmail.com>
2022-04-30 13:52:08 -0300

    meu aprendizado

---
+++ src/pages/meuaprendizado/[curso].tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/layout'
+
+
+
+function Curso() {
+  return (
+    <>
+      <Layout> asdasd </Layout> 
+    </>
+  )
+}
+
+export default Curso
