889e294dcc43710fda97fe719a0847d52176ef2f
joseRuiz <ozur1233@gmail.com>
2022-05-01 15:39:39 -0300

    PagPrincipal.new

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next'
+import Image from 'next/image';
 
 import Layout from "../components/layout";
+import PagPrincipal from '../components/PagPrincipal'
 
 const Home: NextPage = () => {
   return (
     <>
-      <Layout> 
-        Titulo
-      </Layout> 
+      <Layout>
+      </Layout>
+
+      <PagPrincipal></PagPrincipal>
+       
     </>
   )
 }
